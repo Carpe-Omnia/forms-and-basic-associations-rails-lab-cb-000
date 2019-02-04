5415926b12f89a2a0a3ca69783832b9734b37578
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-04 19:31:48 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,9 +5,9 @@ class Song < ActiveRecord::Base
 
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
-  end 
-  def genre_name 
-    self.genre ? self.genre.name : nil 
-  end 
-      
+  end
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
 end
